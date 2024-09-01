a9ca407a0db7ee4d17a97f9ace0c0eb562c5ac86
tinggithub0 <66715785+tinggithub0@users.noreply.github.com>
2024-09-01 21:22:10 +0800

    整理 demo

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # crawler_1
-# labeling_tool
-![labeling_tool](https://user-images.githubusercontent.com/66715785/152665205-5547fa94-09d0-4712-b094-b01d46827b1d.png)
+## captcha_labeling_tool
+![captcha_labeling_tool](https://user-images.githubusercontent.com/66715785/152665205-5547fa94-09d0-4712-b094-b01d46827b1d.png)
+
+## requests_demo
+
+
+## selenium_demo
+
--- selenium_demo.py
+++ selenium_demo.py
@@ -1,4 +1,4 @@
-###  部分內容因涉及公司內部資料予以省略，僅作範例，並將範例中不公開資料以*代替  ###
+### 僅作 demo，部分資料以*代替 ###
 
 from selenium import webdriver
 
@@ -11,11 +11,11 @@ def openChrome():
 def login(driver):
     url = "******"
     driver.get(url)
-    # 找到输入框并输入内容
+    # 找到輸入框並輸入内容
     elem_ID = driver.find_element_by_id("edit-name")
-    elem_ID.send_keys("ID")
+    elem_ID.send_keys("*****")
     elem_PASSWORD = driver.find_element_by_id("edit-pass")
-    elem_PASSWORD.send_keys("password"")
+    elem_PASSWORD.send_keys("*****")
     elem_enter = driver.find_element_by_id("edit-submit")
     elem_enter.click()
 
@@ -23,7 +23,7 @@ def login(driver):
 def send(driver):
     send_work_area = driver.find_element_by_id("*****") # 找到表單問題選項
     send_work_area.click() 
-    ###  中間省略  ###
+    ### 中間省略 ###
     send_submit = driver.find_element_by_name("**") # 填完送出
     send_submit.click()
 
@@ -32,8 +32,3 @@ if __name__ == '__main__':
     driver = openChrome()
     login(driver)
     send(driver)
-
-
-
-
-    
